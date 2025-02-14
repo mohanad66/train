0094d13ca5e3aa4bb947a3be4b5c210de23dfd25
mohanad66 <mohanadmahmoud33245@gmail.com>
2025-02-14 10:01:53 +0200

    score

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+function initializeGame(atkOrDef) {
+    let score = 0;
+    for (let i = 0; i < 3; i++) {
+        if (atkOrDef === 1) {
+            let damage = Math.floor(Math.random() * 10) + 1;
+            if (damage >= 5) {
+                score =score+ 1;
+            } else {
+                score =score- 1;
+            }
+        } else {
+            console.log("nothing happened");
+        }
+    }
+    return {
+        score: score,
+        result: score > 0 ? "Winner" : "Loser"
+    };
+}
+
+const gameResult = initializeGame(1);
+console.log(gameResult);
